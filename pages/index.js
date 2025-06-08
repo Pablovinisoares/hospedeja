d6357dfc138c512ac828e021a6ef66e5f6f2fcf8
Pablovinisoares <pvssoaresphb@gmail.com>
2025-06-07 21:42:28 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,9 +20,7 @@ export default function Home() {
     setSelecao("hospedes");
   };
 
-  // Imagem de fundo (resort e pousada)
-  const imagemFundo =  "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=1350&q=80";
-
+  //  const imagemFundo = "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=1350&q=80";
 
   if (!logged) {
     return (
